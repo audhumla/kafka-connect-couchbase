7351efd3468fd7363c8cb4384b27c92842c5cfbb
dascione <novensides@msn.com>
2018-07-31 08:49:49 +0200

    templates updated

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,49 @@
-FROM 1ambda/kafka-connect:latest
+FROM anapsix/alpine-java:jdk8
+
+# Install kafka
+
+ENV SCALA_VERSION="2.12" \
+    KAFKA_VERSION="0.10.2.1"
+ENV KAFKA_HOME=/opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}
+
+ARG KAFKA_DIST=kafka_${SCALA_VERSION}-${KAFKA_VERSION}
+ARG KAFKA_DIST_TGZ=${KAFKA_DIST}.tgz
+ARG KAFKA_DIST_ASC=${KAFKA_DIST}.tgz.asc
+
+RUN set -x && \
+    apk add --no-cache unzip curl ca-certificates gnupg jq && \
+    eval $(gpg-agent --daemon) && \
+    MIRROR=`curl -sSL https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred'` && \
+    curl -sSLO "${MIRROR}kafka/${KAFKA_VERSION}/${KAFKA_DIST_TGZ}" && \
+    curl -sSLO https://dist.apache.org/repos/dist/release/kafka/${KAFKA_VERSION}/${KAFKA_DIST_ASC} && \
+    curl -sSL  https://kafka.apache.org/KEYS | gpg -q --import - && \
+    gpg -q --verify ${KAFKA_DIST_ASC} && \
+    mkdir -p /opt && \
+    mv ${KAFKA_DIST_TGZ} /tmp && \
+    tar xfz /tmp/${KAFKA_DIST_TGZ} -C /opt && \
+    rm /tmp/${KAFKA_DIST_TGZ} && \
+    apk del unzip curl ca-certificates gnupg
+
+# Set env
+
+ENV PATH=$PATH:/${KAFKA_HOME}/bin \
+    CONNECT_CFG=${KAFKA_HOME}/config/connect-standalone.properties \
+    CB_CONNECT_CFG=${KAFKA_HOME}/config/connector-config.properties \
+    CONNECT_BIN=${KAFKA_HOME}/bin/connect-standalone.sh \
+    CLASSPATH=${KAFKA_HOME}/connectors
+
+ENV JMX_PORT=9999 \
+    CONNECT_PORT=8083
+
+EXPOSE ${JMX_PORT}
+EXPOSE ${CONNECT_PORT}
+
+# Run
+
+WORKDIR $KAFKA_HOME
 COPY target/kafka-connect-couchbase-3.3.2.jar $KAFKA_HOME/connectors
-COPY docker .
+COPY docker/init.sh $KAFKA_HOME
+COPY docker/start.sh $KAFKA_HOME
+COPY config/connector-config.properties $KAFKA_HOME/config/ 
+ENTRYPOINT "exec" "/$KAFKA_HOME/init.sh"
 
-ENTRYPOINT ["/docker-entrypoint.sh"]
-
-CMD ["./start-connect.sh"]
-CMD [init.sh"]
--- docker/init.sh
+++ docker/init.sh
@@ -1,40 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 #Retrieving container ip address for advertising to other Kafka connect workers
 #CONTAINER_IP=`hostname --ip-address`
 CONTAINER_IP=$(tail -1 /etc/hosts | awk '{print $1}')
+# CONTAINER_IP=$(/sbin/ip route|awk '/scope/ { print $9 }')
+echo "CONTAINER_IP: $CONTAINER_IP"
 
 #Setting environment variables for connector > kafka part
-sed -i "s/@CONTAINER_IP@/$CONTAINER_IP/" config/connect-distributed.properties
-sed -i "s/@KAFKA_ADDRESS@/$KAFKA_ADDRESS/" config/connect-distributed.properties
-sed -i "s/@CONNECTOR_TYPE@/$CONNECTOR_TYPE/" config/connect-distributed.properties
+sed -i "s/localhost/$CONTAINER_IP/" $CONNECT_CFG
 
 #Setting environement variables for connector > couchbase part
-sed -i "s/@COUCHBASE_NODES@/$COUCHBASE_NODES/" config/connector-rest-config.json
-sed -i "s/@KAFKA_TOPIC@/$KAFKA_TOPIC/" config/connector-rest-config.json
-sed -i "s/@CONNECTOR_NAME@/$CONNECTOR_NAME/" config/connector-rest-config.json
-sed -i "s/@COUCHBASE_BUCKET@/$COUCHBASE_BUCKET/" config/connector-rest-config.json
-sed -i "s/@CONVERTER_CLASS@/$CONVERTER_CLASS/" config/connector-rest-config.json
-sed -i "s/@FILTER_CLASS@/$FILTER_CLASS/" config/connector-rest-config.json
-sed -i "s/@COUCHBASE_FROM_TO@/$COUCHBASE_FROM_TO/" config/connector-rest-config.json
+sed -i "s/@COUCHBASE_NODES@/$COUCHBASE_NODES/" $CB_CONNECT_CFG
+sed -i "s/@KAFKA_TOPIC@/$KAFKA_TOPIC/" $CB_CONNECT_CFG
+sed -i "s/@CONNECTOR_NAME@/$CONNECTOR_NAME/" $CB_CONNECT_CFG
+sed -i "s/@COUCHBASE_BUCKET@/$COUCHBASE_BUCKET/" $CB_CONNECT_CFG
+sed -i "s/@CONVERTER_CLASS@/$CONVERTER_CLASS/" $CB_CONNECT_CFG
+sed -i "s/@FILTER_CLASS@/$FILTER_CLASS/" $CB_CONNECT_CFG
+sed -i "s/@COUCHBASE_FROM_TO@/$COUCHBASE_FROM_TO/" $CB_CONNECT_CFG
 
+echo "Calling the start.sh of the Kafka Connector"
 
+exec /$KAFKA_HOME/start.sh
 
-# Set configurations
-# First check, in case of restart of Kafka Connect and connector configuration, connector info is already stored in kafka internal topics
-if [[ *${CONNECTOR_NAME}* != $(curl -s -GET localhost:8083/connectors/) ]]; then
-    while [[ $(curl -s -GET localhost:8083/connectors/) != "["* ]] #when the connector is up, the curl response yields connectors name between brackets, ex '[]'or '[Connector-1, Connector-2]'
-    do
-	echo "Waiting connector to be up for configuration"
-	sleep 2
-    done
-    
-    #Random sleep to prevent race
-    sleep $((3 + $RANDOM % 50))
-
-    #Test if connector already created, otherwise create it
-    if [[ *${CONNECTOR_NAME}* != $(curl -s -GET localhost:8083/connectors/) ]]; then
-	curl -XPOST http://localhost:8083/connectors -d "@./config/connector-rest-config.json" -H "Content-Type: application/json"
-    fi
-else
-    echo "Connector already exists"
-fi
